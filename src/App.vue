fd036cf80b69c2c3fee6cda20c9fc3f871d6f41e
Annalisa <annalisadesantis770@gmail.com>
2021-02-11 13:38:50 +0100

    Componente title

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
+     <CustomTitle :title="message" :subtitle="subtitle"/>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="ciao a tutti" />
+    <!-- <HelloWorld msg="ciao a tutti" /> -->
+
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import CustomTitle from "./components/CustomTitle.vue";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    components: {
+      // HelloWorld
+      CustomTitle
+    },
+    data(){
+        return{
+            message: "Pokemon API",
+            subtitle: "Qui troverai tutti i Pokemon"
+
+        }
+    }
+
 };
 </script>
 
---
+++ src/components/CustomTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+      <h1>
+          {{ title }}
+      </h1>
+      <h2>
+          {{ subtitle }}
+      </h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "CustomTitle",
+  props: ['title', 'subtitle']
+};
+</script>
+
+<style scoped>
+h1{
+    color: blue;
+}
+h2{
+    color: pink;
+}
+</style>
